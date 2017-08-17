903078190ddc373fb4cd388b6e228711169c5d81
wans <wanslm@foxmail.com>
2017-08-17 18:29:44 +0800

    update summary

--- BFDisplayEvent.podspec
+++ BFDisplayEvent.podspec
@@ -9,7 +9,7 @@
 Pod::Spec.new do |s|
   s.name             = 'BFDisplayEvent'
   s.version          = '0.0.1'
-  s.summary          = 'A short description of BFDisplayEvent.'
+  s.summary          = '数据显示与点击事件与controller解耦'
 
 # This description is used to generate tags and improve search results.
 #   * Think: What does it do? Why did you write it? What is the focus?
---
+++ UpdatePodScript.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+set -e
+
+deleteTag=0
+commit_msg=""
+
+while  getopts  " d:m: "  arg #选项后面的冒号表示该选项需要参数
+do
+case  $arg  in
+d)
+deleteTag=$OPTARG #是否删除原来tag
+;;
+m)
+commit_msg=$OPTARG
+# echo "d-msg:$commit_msg"
+;;
+? )  #当有不认识的选项的时候arg为 ?
+echo  " unkonw argument "
+exit  1
+;;
+esac
+done
+
+podspecName=$(basename $(find . -name *.podspec) | sed 's/.podspec//g' )
+
+version=$(awk '/\.version/' $podspecName.podspec | awk '/[0-9]\.[0-9]\.[0-9]/' | sed 's/.version//g'  | sed 's/[^0-9/.]//g')
+describe=$(awk '/\.summary/' $podspecName.podspec | sed "s/\'//g" | sed "s/[a-zA-Z]*.summary//g" | sed "s/=//g")
+
+#echo $podspecName
+#echo $describe
+#echo $version
+
+# 检查是否已经提交
+check_results=`git status`
+echo "result: $check_results"
+if [[ $check_results =~ 'nothing to commit' ]];
+then
+  echo "---------Don't Need Commit, Script To Be Continue---------"
+else
+  git add .
+
+  if [ -n "$commit_msg" ];
+  then
+    # commit_msg 不为空
+    git commit -m "$commit_msg"
+    # echo "---------Commiting...,commit_msg:$commit_msg---------"
+  else
+    git commit -m "$describe"
+    # echo "---------Commiting...,describe:$describe---------"
+  fi
+
+
+fi
+
+git pull
+git push
+
+
+if [ $deleteTag = '1' ] ; then
+    git tag --delete $version
+    git push origin --delete tag $version
+fi
+
+
+git tag -a ${version} -m ${version}
+git push origin ${version}
+
+publicSpecs="https://github.com/CocoaPods/Specs.git"
+privateSpecs="https://github.com/wans3112/Specs.git"
+#privateSpecs="http://caohh:123456@192.168.10.224:3000/caohh/iOSPod.git"
+
+#pod repo remove iOSPod LYCache.podspec
+#,http://iOS:123456@192.168.10.224:3000/iOS/iOSPod.git
+
+pod lib lint --sources=$privateSpecs --verbose --use-libraries --allow-warnings
+
+pod repo push WBSpecs $podspecName.podspec --sources=$privateSpecs --use-libraries --allow-warnings
