7034c9d774f4231f042f325f56acd5b401b05351
wans <wanslm@foxmail.com>
2017-09-04 20:15:35 +0800

    1.0.2

--- UpdatePodScript.sh
+++ UpdatePodScript.sh
@@ -73,7 +73,7 @@ privateSpecs="https://github.com/wans3112/Specs.git"
 #pod repo remove iOSPod LYCache.podspec
 #,http://iOS:123456@192.168.10.224:3000/iOS/iOSPod.git
 
-pod lib lint --sources=$publicSpecs,$privateSpecs --verbose --use-libraries --allow-warnings
+pod lib lint --verbose --use-libraries --allow-warnings
 
 # --allow-warnings : 允许 警告，有一些警告是代码自身带的。
 # --use-libraries  : 私有库、静态库引用的时候加上
